ffb3792659bc1f84be41755925b5a1655522fa37
been <s_been@kakao.com>
2022-09-21 02:10:53 +0900

    add: 2720 - 더 좋은 풀이법

--- src/main/kotlin/2720.kt
+++ src/main/kotlin/2720.kt
@@ -98,3 +98,25 @@ class Solution2720 {
 
     private fun inputIntValue() = Integer.parseInt(br.readLine() ?: "0")
 }
+
+/*
+// 더좋은 풀이법
+fun main() {
+    val br = BufferedReader(InputStreamReader(System.`in`))
+    val bw = BufferedWriter(OutputStreamWriter(System.out))
+    val n = br.readLine().toInt()
+    var c = IntArray(n, {br.readLine().toInt()})
+    var change = arrayOf(25, 10, 5, 1)
+
+    for (i in 0 until n) {
+        for (j in 0 until change.size) {
+            bw.write("${c[i] / change[j]} ")
+            c[i] = c[i] % change[j]
+        }
+
+        bw.write("\n")
+    }
+
+    bw.flush()
+}
+*/
